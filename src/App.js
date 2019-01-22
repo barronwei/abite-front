ef06528960c36a8700d32595cd676c6807b19994
Barron Wei <barronwei@outlook.com>
2019-01-22 15:15:49 -0500

    apollo settings

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
 import React, { Component } from 'react'
 import { Switch, Route, BrowserRouter as Router } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components'
+import { ApolloProvider } from 'react-apollo'
 import theme from './theme'
+import config from './config'
 import Home from './containers/Home/'
+import ApolloClient from 'apollo-boost'
+
+// creates new client
+const client = new ApolloClient({
+  uri: config.graphqlUrl
+})
 
 class App extends Component {
   render() {
     return (
       <Router>
         <ThemeProvider theme={theme}>
-          <div className="App">
-            <Switch>
-              <Route path="/" component={Home} />
-            </Switch>
-          </div>
+          <ApolloProvider client={client}>
+            <div className="App">
+              <Switch>
+                <Route path="/" component={Home} />
+              </Switch>
+            </div>
+          </ApolloProvider>
         </ThemeProvider>
       </Router>
     )
