700b4429e15c48e8e27681fdc34a17323cbb5e45
Barron Wei <barronwei@outlook.com>
2019-01-22 12:54:15 -0500

    change styles

---
+++ src/styles.js
@@ -0,0 +1,19 @@
+const Title = styled.h1`
+  font-size: 2.5em;
+  text-align: right;
+  color: coral;
+`;
+const Subtitle = styled.h3`
+  font-size: 1.5em;
+  text-align: center;
+  color: gray;
+`;
+
+const Button = styled.button`
+  color: gray;
+  padding: 2.5rem, 1rem;
+  border: solid 2px coral;
+  border-radius: 3px;
+  margin: 0.5rem;
+  font-size: 1rem;
+`;
